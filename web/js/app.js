f3805c9087005dc5948db0e99b2e344e0f7305d5
ravi <ravi.medde@costrategix.com>
2018-02-17 03:45:59 +0530

    data

--- web/js/app.js
+++ web/js/app.js
@@ -1,4 +1,4 @@
-function init_charts() {
+function init_charts1() {
     if ($("#lineChart").length) {
       var f = document.getElementById("lineChart");
       new Chart(f, {
@@ -6,7 +6,7 @@ function init_charts() {
         data: {
           labels: ["January", "February", "March", "April", "May", "June", "July"],
           datasets: [{
-            label: "My First dataset",
+            label: "Market value",
             backgroundColor: "rgba(38, 185, 154, 0.31)",
             borderColor: "rgba(38, 185, 154, 0.7)",
             pointBorderColor: "rgba(38, 185, 154, 0.7)",
@@ -19,6 +19,8 @@ function init_charts() {
         }
       });
     }
+}
+   function init_charts2() {
     if ($("#mybarChart").length) {
       var f = document.getElementById("mybarChart");
       new Chart(f, {
@@ -26,7 +28,7 @@ function init_charts() {
         data: {
           labels: ["January", "February", "March", "April", "May", "June", "July"],
           datasets: [{
-            label: "# of Votes",
+            label: "% profit per time period",
             backgroundColor: "#26B99A",
             data: [51, 30, 40, 28, 92, 50, 45]
           }]
@@ -42,8 +44,8 @@ function init_charts() {
         }
       })
     }
-  
-  }
+    }
+
 
 $(document).ready(function () {
 $.ajax({url: "", success: function(result){
@@ -54,19 +56,30 @@ $.ajax({url: "", success: function(result){
 $(document).ready(function () {
 $.ajax({url: "", success: function(result){
     // $("#div1").html(result);
+   
 }});
 });
 
-$(document).ready(function () {
-$.ajax({url: "", success: function(result){
-    // $("#div1").html(result);
+// $(document).ready(function () {
+// $.ajax({url: "", success: function(result){
+//     // $("#div1").html(result);
 
-    console.log($(".financial-table tbody tr td"));
-}});
-console.log($(".financial-table tbody tr").children().text("sunil"));
-  });
+//     // console.log($(".financial-table tbody tr td"));
+// }});
+
+// for (let index = 0; index < 3; index++) {
+//     var table_row = document.createElement("tr");
+//     $(".financial-table tbody").append(table_row);
+//     for (let j = 0; j < 7; j++) {
+//         var table_col = document.createElement("td");
+//         console.log($(".financial-table tbody tr").append(table_col).text("sunil"));
+//     }
+// }
+
+
+//   });
 
  $(document).ready(function () {
-    init_charts();
-    init_DataTables();
+    init_charts1();
+    init_charts2();
   });
