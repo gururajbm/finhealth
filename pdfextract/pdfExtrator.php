0f347aa1b6a76ee8423b46b7ab209c03423f11b1
shreyas <shreyas.suresh@costrategix.com>
2018-02-16 23:46:02 +0530

    mysql

--- pdfextract/pdfExtrator.php
+++ pdfextract/pdfExtrator.php
@@ -3,12 +3,28 @@ include('vendor/autoload.php');
 
 $path    = 'pdf/';
 $files = scandir($path);
+$password = 'AJPPG4513Q';
+
+$dbhost = 'localhost:3036';
+$dbuser = 'root';
+$dbpass = 'root';
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 foreach ($files as $file_name) {
 	if (strpos($file_name, '.pdf') !== false) {
-		print_r($file_name);		
+		print_r($file_name.PHP_EOL);
+		exec("qpdf --password='" . $password . "' --decrypt 'pdf/" . $file_name . "' 'decrypted_pdf/temp_" . $file_name . "'");
+
+		$text = \Spatie\PdfToText\Pdf::getText('decrypted_pdf/temp_' . $file_name);
+
+		$sql = 'INSERT INTO transaction_dump'.
+	      '(data`) '.
+	      'VALUES ( ' . $text. ')';
+      
+	   mysql_select_db('fin_health');
+	   $retval = mysql_query( $sql, $conn );		
 	}
 }
 
 
-print_r(\Spatie\PdfToText\Pdf::getText('pdf/output.txt'));
+
