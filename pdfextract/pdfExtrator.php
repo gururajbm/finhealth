bbdff6aa8ab02e6f51be6f8971fc3460542b11e0
shreyas <shreyas.suresh@costrategix.com>
2018-02-17 04:34:49 +0530

    regex methods

--- pdfextract/pdfExtrator.php
+++ pdfextract/pdfExtrator.php
@@ -23,29 +23,62 @@ foreach ($files as $file_name) {
 		echo $sql;
 	   	print_r($sql);
 
-	   	$result = $db->query($sql);
+	   	// $result = $db->query($sql);
+
+	   	get_axis_fund_information($db->realEscapeString($text));
+	   	get_bank_account_number($db->realEscapeString($text));
+	   	get_folio_number($db->realEscapeString($text));
+	   	get_email($text);
+	   	get_mobile($text);
+	   	get_redemption_price($text);
 	}
 }
 
 function get_axis_fund_information($text){
-	preg_match('/(AXIS BANK LTD))/', $text, $matches);
-	if count($matches){
-		print_r(get_fund_name($text));	
-	}
-
+	preg_match("/(AXIS BANK LTD)/", $text, $matches);
+	
+		print_r($matches);
 	
 }
 
-function get_fund_name($text){
-	preg_match('/(?<name>\w+): (?<digit>\d+)/', $str, $matches);
+function get_bank_account_number($text){
+	preg_match("/(Bank Account No.{25})/", $text, $matches);
 
 		print_r($matches);
 }
 
-function get_($text){
-	preg_match('/(?<name>\w+): (?<digit>\d+)/', $str, $matches);
+function get_folio_number($text){
+	preg_match("/(Folio No\. \: \d{10})/", $text, $matches);
 
 		print_r($matches);
+}
+
+function get_email($text){
+	preg_match("/(Email ID \: [.]{10})/", $text, $matches);
+
+		print_r($matches);
+	
+}
+
+function get_mobile($text){
+	preg_match("/(Mobile : \d{10})/", $text, $matches);
+
+		print_r($matches);
+	
+}
+
+function get_purchase($text){
+	preg_match("/(Mobile : \d{10})/", $text, $matches);
+
+		print_r($matches);
+	
+}
+
+function get_redemption_price($text){
+	preg_match("/(Redemption Price - Rs [\d\.]{6})/", $text, $matches);
+
+		print_r($matches);
+	
 }
 
 
